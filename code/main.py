45772992c6bd18c8da0e825d3c15bcdb631b33fc
肖汉骏/Hanjun Xiao <xiaohanjungw@qq.com>
2018-09-23 09:07:49 +0800

    特征提取

--- code/main.py
+++ code/main.py
@@ -17,6 +17,7 @@ class Model(object):
         # Initialize with args
         self.debug = args.debug
         self.path_to_images = args.path_to_images
+        self.path_to_features = args.path_to_features
         self.path_to_dataset_a = args.path_to_dataset_a
         self.path_to_dataset_b = args.path_to_dataset_b
         # Initialize fields
@@ -32,15 +33,15 @@ class Model(object):
 
     def pre_process(self):
         """数据预处理，将输入数据（视频）转换为图片"""
-        self.log('===== PRE-PROCESS START =====')
+        self.log('===== PRE-PROCESSING START =====')
         dataset_paths = [self.path_to_dataset_a, self.path_to_dataset_b]
-        data_types = ['train/', 'test/']
+        data_types = ['train', 'test']
 
         for data_type in data_types:
-            image_type_path = self.path_to_images + data_type
+            image_type_path = self.path_to_images + data_type + '/'
             subprocess.call(['mkdir', '-p', image_type_path])
             for dataset_path in dataset_paths:
-                dataset_type_path = dataset_path + data_type
+                dataset_type_path = dataset_path + data_type + '/'
                 if os.path.isdir(dataset_type_path):
                     dataset_files = os.listdir(dataset_type_path)
                     for file_name in dataset_files:
@@ -64,11 +65,28 @@ class Model(object):
                             subprocess.call(['touch', image_processed_path])
                         else:
                             self.log('[WARN] video %s not processed successfully!' % file_name_no_ext)
-        self.log('===== PRE-PROCESS END =====')
+        self.log('===== PRE-PROCESSING END =====')
 
     def extract_feature(self):
         """提取视频特征"""
-        pass
+        self.log('\n===== FEATURE EXTRACTION START =====')
+        data_types = ['train', 'test']
+        subprocess.call(['mkdir', '-p', self.path_to_features])
+        for data_type in data_types:
+            image_type_path = self.path_to_images + data_type + '/'
+            out_file = self.path_to_features + data_type + '.h5'
+            returncode = subprocess.call(['python', './code/TF_FeatureExtraction/example_feat_extract.py',
+                                          '--network', 'resnet_v2_152', '--checkpoint',
+                                          '/Users/hanjunx/workspace/tensorflow/checkpoints/resnet_v2_152_2017_04_14/resnet_v2_152.ckpt',
+                                          '--image_path', image_type_path,
+                                          '--out_file', out_file,
+                                          '--layer_names', 'resnet_v2_152/logits',
+                                          '--preproc_func', 'inception'])
+            if returncode == 0:
+                self.log('[INFO] feature extraction for %s succeeded!' % data_type)
+            else:
+                self.log('[ERROR] feature extraction for %s failed!' % data_type)
+        self.log('===== FEATURE EXTRACTION END =====')
 
     def train(self):
         pass
@@ -88,6 +106,9 @@ def parse_args():
     parser.add_argument('--path-to-images',
                         default = './data/images/',
                         help = 'Path to images')
+    parser.add_argument('--path-to-features',
+                        default = './data/features/',
+                        help = 'Path to features')
     parser.add_argument('--path-to-dataset-a',
                         default = './data/DatasetA/',
                         help = 'Path to dataset A')
